c79e224c89d6c3a3643ab81bf9c9c599fb9cd2d8
bjfinest98 <opeyemibamigbola54@gmail.com>
2025-07-23 11:23:10 +0100

    commit 2

--- cardealer/car/urls.py
+++ cardealer/car/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
 from . import views 
 
+urlpatterns = [
+    path('', views.home,name="home"),
+    path('about/', views.about,name="about"),
+    path('blogdetails/', views.blogdetails,name="blogdetails"),
+    path('bog/', views.blog,name="home"),
+    path('cardetails/', views.cardetails,name="cardetails"),
+    path('carshtml/', views.carshtml,name="carhtml"),
+    path('contact/', views.contact,name="contact"),
+    path('team/', views.team,name="team"),
+    path('terms/', views.terms,name="terms"),
+    path('testimonials/', views.testimonials,name="testimonials"),
+    
+]
 
 
 
--- cardealer/car/views.py
+++ cardealer/car/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about(request):
+    return render(request, 'about-us.html')
+
+def blogdetails(request):
+    return render(request, 'blog-details.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def cardetails(request):
+    return render(request, 'car-detail.html')
+
+
+def carshtml(request):
+    return render(request, 'cars.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def team(request):
+    return render(request, 'team.html')
+
+
+def terms(request):
+    return render(request, 'terms.html')
+
+
+def testimonials(request):
+    return render(request, 'testimonials.html')
